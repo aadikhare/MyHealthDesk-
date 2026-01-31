86723c384db798fa0b06416bde3b2d341640f9cc
aditya <aadi.khare88@gmail.com>
2026-01-31 15:18:48 +0530

    UserDashbord pom page added

---
+++ src/main/java/com/comcast/crm/objectrepositoryutility/UserDashboardPage.java
@@ -0,0 +1,50 @@
+package com.comcast.crm.objectrepositoryutility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class UserDashboardPage {
+	WebDriver driver;
+	public UserDashboardPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver,this);
+	}
+	
+	@FindBy(xpath="//span[text()='Dashboard']")
+	private WebElement DashbordLink;
+	
+	@FindBy(xpath="//span[text()='Book Appointment']")
+	private WebElement BookAppointmentLink;
+	
+	@FindBy(xpath="//span[contains(text(),'Appointment History')]")
+	private WebElement AppointmentHistoryLink;
+	
+	@FindBy(xpath="//span[contains(text(),'Medical History')]")
+	private WebElement MedicalHistoryLink;
+	
+	@FindBy(xpath="//a[contains(text(),'Update Profile')]")
+	private WebElement UpdateProfileLink ;
+	public WebElement getDashbordLink() {
+		return DashbordLink;
+	}
+
+	public WebElement getBookAppointmentLink() {
+		return BookAppointmentLink;
+	}
+
+	public WebElement getAppointmentHistoryLink() {
+		return AppointmentHistoryLink;
+	}
+
+	public WebElement getMedicalHistoryLink() {
+		return MedicalHistoryLink;
+	}
+
+	public WebElement getUpdateProfileLink() {
+		return UpdateProfileLink;
+	}
+	
+
+}
